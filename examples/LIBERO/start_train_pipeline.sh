fa166a9a409b366ac9c0e7c1fdc7ca56e3dcf169
hansirui_4th <hansirui_4th@dgx-082.cm.cluster>
2026-06-15 20:51:19 +0800

    fix env path

---
+++ examples/LIBERO/start_train_pipeline.sh
@@ -0,0 +1,12 @@
+source /aifs4su/hansirui_4th/miniconda3/bin/activate
+conda activate starVLA
+
+NUM_PROCESSES=8 \
+DATA_MIX=libero_90 \
+RUN_ID=0615_qwenfast_libero90_ep2 \
+MAX_TRAIN_STEPS=4461 \
+SAVE_INTERVAL=8922 \
+BATCH_SIZE=16 \
+WANDB_MODE=offline \
+MAIN_PROCESS_PORT=33376 \
+bash examples/LIBERO/train_files/run_libero_qwenfast_train.sh
